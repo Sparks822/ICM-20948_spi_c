263bef4846f5bd285b7042268337feb64e2a9f0a
Arnold Benedict <69517176+Sparks822@users.noreply.github.com>
2025-03-12 01:04:46 +0800

    Update icm206928_spi.h

--- icm206928_spi.h
+++ icm206928_spi.h
@@ -8,20 +8,11 @@
 #ifndef INC_ICM206928_SPI_H_
 #define INC_ICM206928_SPI_H_
 
-//Bank Select Register 0,1,2,3 on Regitser 7fH or 7F in hexadecimal
+//REG_BANK_SEL, SELECT BANK BEFORE READING/WRITING TO REGISTERS
+#define  SELECT_BANK_REG  0x7F
 
-#define  SELECT_BANK_REG  0x7F //0X7f Base Register SELECT_BANK_REGISTER
-#define  BANK0            0x00 //BIT[5:4] = 0:0 or 0FF , use & AND    Operator to bit 5 and 4 to 0
-#define  BANK1			  0x01 //BIT[5:4] = 0:1 use
-#define  BANK2			  0x10 //BIT[5:4] = 1:0 use
-#define  BANK3			  0x11 //BIT[5:4] = 1:1
-
-//PWR_MGMT_1 , page 37 , Located in bank 0
-
-#define PWR_MGMT_1   0x06  //Register Address
-#define DEVICE_RESET 0x80  //Bit 7 , if set to 1 device will reset = 1000 0000 user OR operator
-#define SLEEP        0X00  //Bit 6 , if set to 0 device will reset = 0000 0000 user & operator
-
+//Read WHO_AMI_REGISTER 
+#define WHO_AM_I 0X00
 
 //Initialize the IMU Sensor
 
