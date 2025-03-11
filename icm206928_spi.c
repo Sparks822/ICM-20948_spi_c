86ca23c854c9fad4e4e11f8d19fecb077a8cd9c7
Arnold Benedict <69517176+Sparks822@users.noreply.github.com>
2025-03-12 01:15:15 +0800

    Update icm206928_spi.c

--- icm206928_spi.c
+++ icm206928_spi.c
@@ -11,5 +11,22 @@
 
 extern SPI_HandleTypeDef hspi1;  // Use the global SPI handle
 
+uint8_t reg;
+uint8_t bank0;
 
+void icm206948_init(){  
+  
+  HAL_Delay(100);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
+  
+  reg = SEL_BANK_REG; //0x7f = 0111 1111 
+  bank0 = 0;
+
+  HAL_SPI_Transmit(&hspi1, &reg1, 1, 100);
+  HAL_SPI_Transmit(&hspi1, &bank0, 1, 100);
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
+  
+
+}
 
