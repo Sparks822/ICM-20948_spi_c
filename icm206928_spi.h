da4ecaa34de04c413bb7cf47240fa492849ded99
Arnold Benedict <69517176+Sparks822@users.noreply.github.com>
2025-03-17 00:11:14 +0800

    Update icm206928_spi.h

--- icm206928_spi.h
+++ icm206928_spi.h
@@ -8,15 +8,24 @@
 #ifndef INC_ICM206928_SPI_H_
 #define INC_ICM206928_SPI_H_
 
-//REG_BANK_SEL, SELECT BANK BEFORE READING/WRITING TO REGISTERS
-#define  SEL_BANK_REG  0x7F
+//Bank Select Register 0,1,2,3 on Regitser 7fH or 7F in hexadecimal
 
-//Read WHO_AMI_REGISTER 
-#define WHO_AM_I 0x00
+#define  REG_BANK_SEL 	0x7F   // Register for selecting the bank
+#define  bank_0 			0x00   // Bank 0 value (0000 0000)
+#define  bank_1 			0x10   // Bank 1 value (0001 0000)
+#define  bank_2 			0x20   // Bank 2 value (0010 0000)
+#define  bank_3 			0x30   // Bank 3 value (0011 0000)
+
+
+//PWR_MGMT_1 , page 37 , Located in bank 0
+
+#define PWR_MGMT_1    0x06   //Register Address
+#define WHO_AM_I     0X00  //
 
 //Initialize the IMU Sensor
 
-void icm206948_init();
+void select_bank0();
+void who_am_i();
 
 
 #endif /* INC_ICM206928_SPI_H_ */
